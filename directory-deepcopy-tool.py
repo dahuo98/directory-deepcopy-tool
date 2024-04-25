b2676a18ecd4f3eb6e12bb5abedc0fd3ffe784f0
dahuo98 <sxdahuo@gmail.com>
2024-04-25 01:01:58 -0400

    implemented copy file

--- directory-deepcopy-tool.py
+++ directory-deepcopy-tool.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+
 
 def ask_to_proceed():
     answer = input("Continue? (y/n) \n")
@@ -22,10 +24,29 @@ def get_directory_path_from_commandline(prompt: str) -> str:
     ask_to_proceed()
     return path
 
+def copy_file(source_path: str, dest_path: str) -> None:
+    if not os.path.isfile(source_path):
+        print("ERROR: {path} is not a file!".format(path = source_path))
+        exit(1)
+
+    if os.path.isfile(dest_path):
+        print("ERROR: {path} already exists and is a file!".format(path = dest_path))
+        exit(1)
+
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
+
+    shutil.copy(source_path, dest_path)
+
+
 
 if __name__ == '__main__':
 
-    source_path = get_directory_path_from_commandline("source path: \n")
-    destination_path = get_directory_path_from_commandline("destination path: \n")
+    # source_path = get_directory_path_from_commandline("source path: \n")
+    # destination_path = get_directory_path_from_commandline("destination path: \n")
+    source_path = input("source path: \n")
+    destination_path = input("destination path: \n")
+    print("copying {source} to {dest}".format(source=source_path, dest=destination_path))
+    copy_file(source_path, destination_path)
 
 
