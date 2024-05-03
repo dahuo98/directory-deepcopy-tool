58033be5ed784f28e8f2024ae81f0296e40ba9de
dahuo98 <sxdahuo@gmail.com>
2024-05-02 21:47:42 -0400

    implement merge copy directory

--- directory-deepcopy-tool.py
+++ directory-deepcopy-tool.py
@@ -28,8 +28,10 @@ def get_directory_path_from_commandline(prompt: str) -> str:
 def check_destination_file_exists(source_file_path: str, dest_directory_path: str) -> None:
     base_file_name = os.path.basename(source_file_path)
     dest_file_path = os.path.join(dest_directory_path, base_file_name)
-    print("destination file {dest_file} already exists, continue will overwrite".format(dest_file=dest_file_path))
-    ask_to_proceed()
+
+    if os.path.exists(dest_file_path):
+        print("destination file {dest_file} already exists, continue will overwrite".format(dest_file=dest_file_path))
+        ask_to_proceed()
 
 
 """
@@ -45,6 +47,8 @@ Returns:
 
 
 def copy_file(source_path: str, dest_path: str) -> int:
+    print("copying file {source} to {dest}".format(source=source_path, dest=dest_path))
+
     if not os.path.isfile(source_path):
         print("ERROR: {path} is not a file!".format(path=source_path))
         exit(1)
@@ -63,10 +67,73 @@ def copy_file(source_path: str, dest_path: str) -> int:
     return num_bytes_copied
 
 
+"""
+Copy all content under source_directory_path to under dest_directory_path.
+
+Content will be merged into dest_directory_path. That is when copying source directory to destination directory,
+the file structure will be preserved. Furthermore, if destination directory already exists, file will be put under
+the desire directory instead of overwrite the entire directory
+
+Args:
+    source_directory_path: path to source directory
+    dest_directory_path: path of destination directory
+    
+Returns:
+    int: number of bytes copied
+
+example:
+source: /common/path/path/to/file.txt
+destination: /common/path/
+
+destination before copy:
+|--common
+|    |
+|    |--path
+|    |    |
+|    |    |-path
+|    |    |   |
+|    |    |   |--to
+|    |    |   |   |
+|    |    |   |   |--old_file1.txt
+|    |    |   |   |--old_file2.txt
+
+-----------------------------------------------------
+
+destination after copy:
+|--common
+|    |
+|    |--path
+|    |    |
+|    |    |-path
+|    |    |   |
+|    |    |   |--to
+|    |    |   |   |
+|    |    |   |   |--old_file1.txt
+|    |    |   |   |--old_file2.txt
+|    |    |   |   |--file.txt
+"""
+
+
+def merge_copy_directory(source_directory_path: str, dest_directory_path: str) -> int:
+    print('merge copying directory {source} to {dest}'.format(source=source_directory_path, dest=dest_directory_path))
+
+    for source_entry_name in os.listdir(source_directory_path):
+
+        abs_source_entry_path = os.path.join(source_directory_path, source_entry_name)
+        abs_dest_entry_path = os.path.join(dest_directory_path, source_entry_name)
+
+        if os.path.isfile(abs_source_entry_path):
+            copy_file(abs_source_entry_path, dest_directory_path)
+        elif os.path.isdir(abs_source_entry_path):
+            merge_copy_directory(abs_source_entry_path, abs_dest_entry_path)
+        else:
+            print("should not be here")
+            exit(1)
+
+
 if __name__ == '__main__':
     # source_path = get_directory_path_from_commandline("source path: \n")
     # destination_path = get_directory_path_from_commandline("destination path: \n")
     source_path = input("source path: \n")
     destination_path = input("destination path: \n")
-    print("copying {source} to {dest}".format(source=source_path, dest=destination_path))
-    copy_file(source_path, destination_path)
+    merge_copy_directory(source_path, destination_path)
